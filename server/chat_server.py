cf5b5f6a9c9fc420a731cd7793aeaf49a6848199
Nick Mezhnyakov <mezhng@gmail.com>
2023-12-10 14:38:22 +0300

    Remake to websocket server.

--- server/chat_server.py
+++ server/chat_server.py
@@ -1,5 +1,9 @@
 import asyncio
+import json
+
 import logging
+
+import websockets
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
@@ -9,62 +13,87 @@ logger.debug('Logger initiated!')
 clients = {}  # remake to redis?
 
 
-async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    username = await connect_user_to_chat(writer)
+async def handle_client(websocket: websockets.WebSocketClientProtocol):
+    """
+    Protocol of client handling.
+    First message from client should be dict containing username and text "connecting".
+    Server responds with "connected".
+    After that messaging can be made.
+    System commands must be sent in key "command".
+
+    Examples of messages:
+    {"username": "Ivan", "message": "connecting"} # from client who wants to connect
+    {"message": "hello"} # regular message from client
+    {"command": "room switch 2"} # command to switch room (for future)
+    """
+    to_run = True
+    while to_run:
+        try:
+            client = websocket.remote_address
+            data = await websocket.recv()
+            message = json.loads(data)
+        except websockets.ConnectionClosedOK:
+            if client in clients.keys():
+                del clients[client]
+                logger.debug(f'Client {client} disconnected')
+            continue
+
+        except Exception as e:
+            logger.warning(f'Caught error: {e}')
+            continue
+
+        if client in clients.keys():
+            await handle_messaging(websocket, message)
+            await handle_commands()
+            await handle_system()
+        else:
+            await connect_user_to_chat(websocket, message)
+
+
+async def connect_user_to_chat(websocket: websockets.WebSocketClientProtocol, message: dict):
     try:
-        await handle_messaging(reader, writer, username)
-    except asyncio.CancelledError:
-        pass
-    except Exception as e:
-        logger.warning(f'Caught error: {e}')
+        username = message['username']
+        if message['message'] == 'connecting':
+            logger.debug(f'User {username} connected')
+        else:
+            logger.warning('No connecting message, dropping')
+    except KeyError:
+        logger.warning('Bad connect message got from user, dropping')
     finally:
-        del clients[username]
-        for client_writer in clients.values():
-            client_writer.write(f'{username} disconnected'.encode())
-            await client_writer.drain()
-        writer.close()
-        await writer.wait_closed()
-        logger.debug(f'{username} disconnected')
+        logger.debug(message)
+
+    await websocket.send(json.dumps({'system': 'connected'}))
+    for client in clients.values():
+        client.send('Another user connected!')
+
+    clients[websocket.remote_address] = websocket
 
 
-async def connect_user_to_chat(writer: asyncio.StreamWriter) -> str:
-    host, port = writer.get_extra_info('peername')
-    username = f'{host}:{port}'
-    logger.debug(f'User {username} connected')
-    writer.write(b'You can start chatting')
-    await writer.drain()
-    for client_writer in clients.values():
-        client_writer.write('Another user connected!'.encode())
-        await client_writer.drain()
-    clients[username] = writer
-    return username
+async def handle_commands():
+    pass
 
 
-async def handle_messaging(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, username: str):
-    while True:
-        data = await reader.read(100)
-        message = data.decode()
-        if not message:
-            break
+async def handle_system():
+    pass
 
-        logger.debug(f'{username}: {message.strip()}')
 
-        # broadcasting?
-        for client, client_writer in clients.items():
-            if client_writer != writer:
-                client_writer.write(f'{username}: {message}'.encode())
-                await client_writer.drain()
+async def handle_messaging(websocket: websockets.WebSocketClientProtocol, message: dict):
+    try:
+        message = message['message']
+    except KeyError:
+        return
+
+    logger.debug(f'{websocket.remote_address}: {message}')
+
+    # broadcasting?
+    for client, ws in clients.items():
+        if ws != websocket:
+            ws.send(f'{websocket.remote_address}: {message}')
 
 
 async def main():
-    server = await asyncio.start_server(
-        handle_client,
-        'localhost',
-        1234
-    )
-
-    async with server:
-        await server.serve_forever()
+    async with websockets.serve(handle_client, "localhost", 4321) as server:
+        await asyncio.Future()
 
 
 if __name__ == '__main__':
