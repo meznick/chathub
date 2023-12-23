ea64d709db2535e1f3515888983436fea7dfc3a1
Nick Mezhnyakov <mezhng@gmail.com>
2023-12-23 17:13:45 +0300

    Add authStore and update generalStore
    A new store, authStore, has been added to handle authentication related data. Simultaneously, the existing generalStore has been updated to include application settings.

---
+++ client/src/stores/auth.ts
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia';
+
+export const useAuthStore = defineStore('authStore', () => {
+  // username
+  // email
+  // password1
+  // password2
+  // token
+});
--- client/src/stores/general.ts
+++ client/src/stores/general.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
 
 export const useGeneralStore = defineStore('generalStore', () => {
-
+  // app settings
 });
