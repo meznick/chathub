5814f78066f8f60b7ea2014316cfc9030164854a
Nick Mezhnyakov <mezhng@gmail.com>
2023-12-23 17:13:37 +0300

    Add router to Vue app
    The main.ts file has been updated to import and use the router from the routes.ts file. The router is then used in the Vue app, allowing the app to handle different routes.

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia";
 import './style.css'
 import App from './App.vue'
+import {router} from "../routes.ts";
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(router)
 app.mount('#app')
