889bd95c0181f9e5b70821ec9a80464a933632e4
Nick Mezhnyakov <mezhng@gmail.com>
2023-12-23 17:13:27 +0300

    Add Vue router configuration
    A new 'routes.ts' file has been created, which includes the configuration for the Vue router. This configuration handles the routing for three components: Chat, Auth, and UserProfile.

---
+++ client/routes.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Chat from "./src/components/Chat.vue";
+import Auth from "./src/components/Auth.vue";
+import UserProfile from "./src/components/UserProfile.vue"
+
+const routes = [
+  { path: "/", component: Chat },
+  { path: "/login", component: Auth},
+  { path: "/user", component: UserProfile },
+];
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+});
