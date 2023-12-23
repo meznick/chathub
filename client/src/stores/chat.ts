37c746fc3c3db88733fa8225ede952c5b59113ee
Nick Mezhnyakov <mezhng@gmail.com>
2023-12-23 16:41:24 +0300

    Update chat store functionality
    The Message type has been updated to include an author_id. Accordingly, the saveMessage function now takes an author_id as a parameter, which gets pushed to the messages array along with the id and text fields.

--- client/src/stores/chat.ts
+++ client/src/stores/chat.ts
@@ -1,20 +1,22 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-type Message = {
+export type Message = {
     id: number;
     text: string;
+    author_id: string;
 };
 
 export const useChatStore = defineStore('chatStore', () => {
     const messages = ref<Message[]>([])
     const messagesCounter = ref<number>(0)
     const newMessage = ref<string>('')
-    function saveMessage(){
+    function saveMessage(author_id: string){
       if (newMessage.value.trim() === '') return;
       messages.value.push({
           id: messagesCounter.value,
-          text: newMessage.value.trim()
+          text: newMessage.value.trim(),
+          author_id: author_id,
       });
       messagesCounter.value++;
       newMessage.value = '';
