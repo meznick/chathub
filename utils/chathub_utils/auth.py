71797f1725f5f31b8cced06b8da29bd086d75dcf
Nick Mezhnyakov <mezhng@gmail.com>
2024-01-07 21:01:45 +0300

    Implement advanced authentication processing
    The commit enhances the AuthProcessor class by implementing more advanced and secure authentication processing. This includes adding relevant dependencies, setting up logging, and modifying the class constructor. Additionally, it checks the password against the hashed password from the database during authentication, and improves the token generation and verification mechanisms.

--- utils/chathub_utils/auth.py
+++ utils/chathub_utils/auth.py
@@ -1,18 +1,42 @@
+import logging
 import re
+from datetime import datetime, timedelta
 from typing import Optional
+
+import jwt
+from argon2 import PasswordHasher
+from argon2.profiles import RFC_9106_LOW_MEMORY
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.StreamHandler())
+LOGGER.setLevel(logging.DEBUG)
+
+DATETIME_DUMP_FORMAT = '%Y-%m-%dT%H:%M:%S'
+TOKEN_EXPIRATION_TIME_HOURS = 2
+TOKEN_REFRESH_DELTA_MINS = 60
 
 
 class AuthProcessor:
-    def __init__(self, redis_connector, secret: str, algorithm: Optional[str] = 'HS256'):
+    def __init__(
+            self,
+            redis_connector,
+            postgres_connector,
+            secret: str,
+            algorithm: Optional[str] = 'HS256'
+    ):
         # todo: create some interface for redis_connector type annotation
         self._redis_connector = redis_connector
+        self._postgres_connector = postgres_connector
         self._secret = secret
         self._algorithm = algorithm
+        self._password_hasher = PasswordHasher.from_parameters(RFC_9106_LOW_MEMORY)
+        LOGGER.info('Auth processor initialized')
 
     def get_algo(self):
         return self._algorithm
 
     def validate_credentials(self, username: str, password: str) -> bool:
+        LOGGER.debug('Validating credentials')
         return (
             self.validate_username(username)
             and self.validate_password(password)
@@ -45,11 +69,44 @@ class AuthProcessor:
         pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
         return bool(re.fullmatch(pattern, password))
 
-    def generate_token(self, username: str, password: str) -> str:
-        ...
+    async def authenticate(self, username: str, password: str) -> bool:
+        # get user from pg
+        user = await self._postgres_connector.get_user(username)
+        if not user:
+            LOGGER.debug('User does not exist')
+            return False
+        # compare hash from db with provided password's hash
+        saved_hash = user.get('password_hash')
+        if not self._password_hasher.verify(saved_hash, password):
+            LOGGER.debug('Password does not match saved hash')
+            return False
+        return True
 
-    def verify_token(self, token: str) -> bool:
-        ...
+    def generate_token(self, username: str) -> str:
+        payload = {
+            'username': username,
+            'expire_time': (
+                    datetime.utcnow() +
+                    timedelta(hours=TOKEN_EXPIRATION_TIME_HOURS)
+            ).strftime(DATETIME_DUMP_FORMAT)
+        }
+        # put to cache with 50m expiration
+        return jwt.encode(payload, key=self._secret, algorithm=self._algorithm)
+
+    def verify_token(self, token: str) -> Optional[str]:
+        # get from cache - if exists then do not make full verification
+        payload = jwt.decode(token, key=self._secret, algorithms=[self._algorithm])
+        if not payload:
+            return
+        elif datetime.strptime(payload['expire_time'], DATETIME_DUMP_FORMAT) < datetime.utcnow():
+            return
+        elif (
+            datetime.strptime(payload['expire_time'], DATETIME_DUMP_FORMAT)
+            - datetime.utcnow() < timedelta(minutes=TOKEN_REFRESH_DELTA_MINS)
+        ):
+            return self.generate_token(payload['username'])
+        else:
+            return token
 
     def update_token_expiration(self, token: str) -> bool:
         ...
