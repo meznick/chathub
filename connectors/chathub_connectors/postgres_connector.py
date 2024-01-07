a3915610abc04ce671f1995ed602efb042743f1f
Nick Mezhnyakov <mezhng@gmail.com>
2024-01-07 21:01:14 +0300

    Add logging and user handling to Postgres connector
    Enhanced the Postgres connector with logging functionality and implemented methods to handle user-related operations. Also added default values for connection parameters (host, port, db). Besides, the connector's destruction process is improved to handle cases where there's no active client to disconnect.

--- connectors/chathub_connectors/postgres_connector.py
+++ connectors/chathub_connectors/postgres_connector.py
@@ -1,15 +1,21 @@
 import asyncio
 from typing import Optional
+import logging
 
 import asyncpg
+from asyncpg import Record
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.StreamHandler())
+LOGGER.setLevel(logging.DEBUG)
 
 
 class AsyncPgConnector:
     def __init__(
             self,
-            host: str,
-            port: int,
-            db: int,
+            host: str = 'localhost',
+            port: int = 5432,
+            db: int = 'chathub_dev',
             username: Optional[str] = None,
             password: Optional[str] = None,
     ):
@@ -19,6 +25,7 @@ class AsyncPgConnector:
         self._username = username
         self._password = password
         self.client = None
+        LOGGER.info('PG connector initialized')
 
     async def connect(self):
         self.client = await asyncpg.connect(
@@ -28,6 +35,49 @@ class AsyncPgConnector:
             user=self._username,
             password=self._password,
         )
+        LOGGER.info(f'Connected to {self._host}:{self._port}:{self._db}')
+
+    async def get_user(self, username: str) -> Optional[Record]:
+        """
+        :param username: The username of the user to fetch.
+        :return: The user data fetched from the database.
+        """
+        if not self.client:
+            await self.connect()
+
+        query = 'SELECT * FROM users WHERE username = $1;'
+        data = await self.client.fetchrow(query, username)
+        LOGGER.debug(f'Fetched user: {data}')
+        return data
+
+    async def add_user(
+            self,
+            username: str,
+            password_hash: str,
+            avatar_url: Optional[str] = None,
+            bio: Optional[str] = None,
+            sex: Optional[str] = None,
+            name: Optional[str] = None,
+            rating: Optional[float] = 0.0
+    ):
+        pass
+
+    async def update_user(
+            self,
+            username: str,
+            password_hash: Optional[str] = None,
+            avatar_url: Optional[str] = None,
+            bio: Optional[str] = None,
+            sex: Optional[str] = None,
+            name: Optional[str] = None,
+            rating: Optional[float] = None
+    ):
+        pass
 
     def __del__(self):
-        asyncio.new_event_loop().run_until_complete(self.client.close())
+        if self.client:
+            # looks like it cannot close properly
+            asyncio.new_event_loop().run_until_complete(self.client.close())
+            LOGGER.info(f'PG connection to {self._host}:{self._port}:{self._db} closed')
+
+        LOGGER.debug('PG connector deleted')
