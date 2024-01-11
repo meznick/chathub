84b26f8188080f807070cd74059f8f36ae04e668
Nick Mezhnyakov <mezhng@gmail.com>
2024-01-11 17:53:13 +0300

    Expand AuthProcessor unittests in test_auth.py
    Several new unittests were added to `test_auth.py` to test for user authentication. The tests include validating the existence of a user, checking if the input password matches with the stored password hash, and a test for successful user authentication. A test setup and teardown for event loop has also been added.

--- utils/tests/test_auth.py
+++ utils/tests/test_auth.py
@@ -1,9 +1,33 @@
+import asyncio
+import logging
 import unittest
+from unittest.mock import MagicMock, AsyncMock
+
+from argon2.exceptions import InvalidHashError
 
 from chathub_utils.auth import AuthProcessor
 
 
-class TestUsernameValidation(unittest.TestCase):
+class TestAuthProcessor(unittest.TestCase):
+    def setUp(self):
+        self.mock_postgres_connector = MagicMock()
+        self.mock_password_hasher = MagicMock()
+        self.mock_redis_connector = MagicMock()
+        self.authenticate = AuthProcessor(
+            self.mock_redis_connector,
+            self.mock_postgres_connector,
+            self.mock_password_hasher,
+            'test',
+            'HS256',
+            logging.ERROR
+        )
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+    def tearDown(self):
+        self.loop.close()
+        asyncio.set_event_loop(None)
+
     def test_validate_username(self):
         validate_function = AuthProcessor._validate_username
 
@@ -32,4 +56,32 @@ class TestUsernameValidation(unittest.TestCase):
         # Test case 6: Password with no special characters
         self.assertFalse(validate_function('ABCabc123'))
 
-    # todo: add tests for auth class methods
+    def test_authenticate_user_does_not_exist(self):
+        self.mock_postgres_connector.get_user = AsyncMock(return_value=None)
+
+        res = self.loop.run_until_complete(self.authenticate._authenticate('user1', 'password1'))
+
+        self.assertFalse(res)
+        self.mock_postgres_connector.get_user.assert_called_once_with('user1')
+
+    def test_authenticate_password_does_not_match(self):
+        user_dict = {'password_hash': 'wrong_hash'}
+        self.mock_postgres_connector.get_user = AsyncMock(return_value=user_dict)
+        self.mock_password_hasher.verify.side_effect = InvalidHashError('Bad password')
+
+        res = self.loop.run_until_complete(self.authenticate._authenticate('user2', 'password2'))
+
+        self.assertFalse(res)
+        self.mock_postgres_connector.get_user.assert_called_once_with('user2')
+        self.mock_password_hasher.verify.assert_called_once_with('wrong_hash', 'password2')
+
+    def test_authenticate_successful(self):
+        user_dict = {'password_hash': 'good_hash'}
+        self.mock_postgres_connector.get_user = AsyncMock(return_value=user_dict)
+        self.mock_password_hasher.verify.return_value = True
+
+        res = self.loop.run_until_complete(self.authenticate._authenticate('user3', 'password3'))
+
+        self.assertTrue(res)
+        self.mock_postgres_connector.get_user.assert_called_once_with('user3')
+        self.mock_password_hasher.verify.assert_called_once_with('good_hash', 'password3')
