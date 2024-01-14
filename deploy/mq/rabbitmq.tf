dc78a9b221c6e1fba68a5f42f8b0f73b135dc784
Nick Mezhnyakov <mezhng@gmail.com>
2024-01-14 20:51:13 +0300

    Refactor RabbitMQ Terraform config for dev/prod split
    This commit modifies the RabbitMQ Terraform configuration to separate concerns between development and production environments. The configuration for vhost, exchange, queue, and binding resources have each been updated to delineate between these environments. This should improve the organization and management of our RabbitMQ infrastructure across different development stages.

--- deploy/mq/rabbitmq.tf
+++ deploy/mq/rabbitmq.tf
@@ -26,19 +26,14 @@ variable "rabbitmq_endpoint" {
   default = "http://localhost:8081"
 }
 
-variable "target_vhost" {
-  type = string
-  default = "chathub_prod"
-}
-
 provider "rabbitmq" {
   endpoint = var.rabbitmq_endpoint
   username = var.provider_user
   password = var.provider_password
 }
 
-resource "rabbitmq_vhost" "chathub_prod" {
-  name = var.target_vhost
+resource "rabbitmq_vhost" "chathub" {
+  name = "chathub"
 }
 
 resource "rabbitmq_user" "api_user" {
@@ -47,9 +42,9 @@ resource "rabbitmq_user" "api_user" {
   tags     = ["service"]
 }
 
-resource "rabbitmq_permissions" "permissions" {
+resource "rabbitmq_permissions" "permissions_prod" {
   user  = rabbitmq_user.api_user.name
-  vhost = rabbitmq_vhost.chathub_prod.name
+  vhost = rabbitmq_vhost.chathub.name
 
   permissions {
     configure = "^$"
@@ -58,9 +53,9 @@ resource "rabbitmq_permissions" "permissions" {
   }
 }
 
-resource "rabbitmq_exchange" "direct_main" {
-  name  = "direct_main"
-  vhost = rabbitmq_vhost.chathub_prod.name
+resource "rabbitmq_exchange" "direct_main_prod" {
+  name  = "direct_main_prod"
+  vhost = rabbitmq_vhost.chathub.name
 
   settings {
     type        = "direct"
@@ -69,9 +64,20 @@ resource "rabbitmq_exchange" "direct_main" {
   }
 }
 
-resource "rabbitmq_queue" "matchmaker" {
-  name  = "matchmaker"
-  vhost = rabbitmq_vhost.chathub_prod.name
+resource "rabbitmq_exchange" "direct_main_dev" {
+  name  = "direct_main_dev"
+  vhost = rabbitmq_vhost.chathub.name
+
+  settings {
+    type        = "direct"
+    durable     = false
+    auto_delete = false
+  }
+}
+
+resource "rabbitmq_queue" "matchmaker_prod" {
+  name  = "matchmaker_prod"
+  vhost = rabbitmq_vhost.chathub.name
 
   settings {
     durable     = false
@@ -82,10 +88,31 @@ resource "rabbitmq_queue" "matchmaker" {
   }
 }
 
-resource "rabbitmq_binding" "matchmaker" {
-  source           = rabbitmq_exchange.direct_main.name
-  vhost            = rabbitmq_vhost.chathub_prod.name
-  destination      = rabbitmq_queue.matchmaker.name
+resource "rabbitmq_queue" "matchmaker_dev" {
+  name  = "matchmaker_dev"
+  vhost = rabbitmq_vhost.chathub.name
+
+  settings {
+    durable     = false
+    auto_delete = false
+    arguments = {
+      "x-queue-type" : "classic",
+    }
+  }
+}
+
+resource "rabbitmq_binding" "matchmaker_prod" {
+  source           = rabbitmq_exchange.direct_main_prod.name
+  vhost            = rabbitmq_vhost.chathub.name
+  destination      = rabbitmq_queue.matchmaker_prod.name
+  destination_type = "queue"
+  routing_key      = "matchmaker"
+}
+
+resource "rabbitmq_binding" "matchmaker_dev" {
+  source           = rabbitmq_exchange.direct_main_dev.name
+  vhost            = rabbitmq_vhost.chathub.name
+  destination      = rabbitmq_queue.matchmaker_dev.name
   destination_type = "queue"
   routing_key      = "matchmaker"
 }
