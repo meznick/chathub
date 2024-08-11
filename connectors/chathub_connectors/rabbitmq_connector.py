a8e09bdf6d0a54b5e3e37be068ba8789668064d2
Nick Mezhnyakov <mezhng@gmail.com>
2024-08-11 11:32:35 +0300

    Add detailed docstrings and consolidate RabbitMQConnector
    Extended docstrings for the `RabbitMQConnector` class and its `__init__` method with detailed descriptions and usage examples. Added a `run` method and modified `_publish` to `publish`; incorporated `caller_service` into log messages for better traceability.

--- connectors/chathub_connectors/rabbitmq_connector.py
+++ connectors/chathub_connectors/rabbitmq_connector.py
@@ -15,6 +15,33 @@ LOGGER.addHandler(stream_handler)
 
 
 class RabbitMQConnector:
+    """
+    This class provides a connector to the RabbitMQ server for asynchronous messaging.
+
+    Each instance of the `RabbitMQConnector` can connect to a RabbitMQ server,
+    bind to specified exchanges and queues, send and receive messages.
+
+    Class attributes include connection details like `host`, `port`, `virtual_host`,
+    `exchange`, `queue`, `routing_key`, `username`, `password` and `message_callback`.
+    The class also has a `call_service` attribute that indicates the name of the service
+    using the RabbitMQ connector.
+
+    Messages are published using the `publish` method, and received messages are
+    handled by the `message_callback` function if provided, or the default_on_message_callback
+    method if not.
+
+    Example:
+    init_params = {
+        'host': 'localhost',
+        'port': 5672,
+        'virtual_host': 'test',
+        'callback_method': lambda x: print(x),
+    }
+    connector = RabbitMQConnector(**init_params)
+    connector.run() # it's async
+    connector.publish('message', 'some_key', 'exchange_name')
+    """
+
     def __init__(
             self,
             host: str = 'localhost',
@@ -26,8 +53,26 @@ class RabbitMQConnector:
             username: Optional[str] = None,
             password: Optional[str] = None,
             message_callback: Callable = None,
+            caller_service: str = '',
             loglevel: Optional[str] = logging.DEBUG,
     ):
+        """
+        Initialize the RabbitMQ connector.
+
+        :param host: The host name or IP address of the RabbitMQ server.
+                     Default is 'localhost'.
+        :param port: The port number of the RabbitMQ server. Default is 8082.
+        :param virtual_host: The virtual host name to connect to. Default is an empty string.
+        :param exchange: The exchange name to bind to. Default is an empty string.
+        :param queue: The queue name to bind to. Default is an empty string.
+        :param routing_key: The routing key used for message routing. Default is an empty string.
+        :param username: The username for authentication. Default is None.
+        :param password: The password for authentication. Default is None.
+        :param message_callback: A callback function to handle received messages. Default is None.
+        :param caller_service: The name of the service using the RabbitMQ connector.
+                     Default is an empty string.
+        :param loglevel: The logging level for the RabbitMQ connector. Default is logging.DEBUG.
+        """
         LOGGER.setLevel(loglevel)
         creds = PlainCredentials(
             username=username,
@@ -51,7 +96,11 @@ class RabbitMQConnector:
         self._routing_key = routing_key
         self._message_callback = message_callback
         self._channel = None
-        LOGGER.info('RabbitMQ connector initialized')
+        self.tag = f'python-rmq-connector-{caller_service}'
+        LOGGER.info(f'RabbitMQ connector {self.tag} initialized')
+
+    def run(self):
+        LOGGER.info(f'Running RabbitMQ connector {self.tag}')
         try:
             if not self._connection.ioloop.is_running():
                 LOGGER.debug('Starting ioloop')
@@ -65,7 +114,7 @@ class RabbitMQConnector:
             self._connection.close()
             LOGGER.info('Connection closed')
 
-    def _publish(self, message: str, routing_key: str, exchange: str):
+    def publish(self, message: str, routing_key: str, exchange: str):
         if self._channel.is_open:
             self._channel.basic_publish(exchange=exchange, routing_key=routing_key, body=message)
             LOGGER.debug(
@@ -91,9 +140,10 @@ class RabbitMQConnector:
                 if self._message_callback
                 else self._default_on_message_callback
             ),
+            consumer_tag=self.tag,
             auto_ack=True
         )
-        LOGGER.debug('RMQ channel opened')
+        LOGGER.debug(f'Consuming on queue {self._queue}')
 
     @staticmethod
     def _default_on_message_callback(channel, method, properties, body):
