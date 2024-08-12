b6ab682b16c44760b6ae786ed937ff28c664627c
Nick Mezhnyakov <mezhng@gmail.com>
2024-08-12 19:23:46 +0300

    Rename and update test_router to dev_router
    Renamed test_router.py to dev_router.py and modified the corresponding router name and references. Updated the docstring to emphasize that the router is for development purposes only.

--- chathub_bot/bot/dev_router.py
+++ chathub_bot/bot/dev_router.py
@@ -4,16 +4,20 @@ from aiogram.types import Message
 
 from . import LOGGER
 
-test_router = Router(name='__name__')
+"""
+Router contains commands only for development purposes. Need to make sure
+that users don't have access to these.
+"""
+dev_router = Router(name='__name__')
 
 
-@test_router.message(CommandStart())
+@dev_router.message(CommandStart())
 async def handle_start(message: Message):
     LOGGER.debug(f'Got start command from {message.from_user.username}')
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}, welcome to the bot!")
 
 
-@test_router.message()
+@dev_router.message()
 async def echo(message: Message):
     LOGGER.debug(f'Got message from {message.from_user.username}')
     await message.send_copy(chat_id=message.chat.id)
