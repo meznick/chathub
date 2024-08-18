7a47d00b6c70d6a112db038d60b7df08ca1274fd
Nick Mezhnyakov <mezhng@gmail.com>
2024-08-18 15:52:04 +0300

    Remove handle_start function from dev_router.py
    Deleted the handle_start function that processed the /start command. This cleanup keeps the router focused on echo functionality without handling initial start commands.

--- chathub_bot/bot/dev_router.py
+++ chathub_bot/bot/dev_router.py
@@ -11,12 +11,6 @@ that users don't have access to these.
 dev_router = Router(name='__name__')
 
 
-@dev_router.message(CommandStart())
-async def handle_start(message: Message):
-    LOGGER.debug(f'Got start command from {message.from_user.username}')
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}, welcome to the bot!")
-
-
 @dev_router.message()
 async def echo(message: Message):
     LOGGER.debug(f'Got message from {message.from_user.username}')
