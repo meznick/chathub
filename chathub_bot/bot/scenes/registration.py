831ea55ddea1b07e3286d77265163909829c22f5
Nick Mezhnyakov <mezhng@gmail.com>
2024-08-19 21:13:14 +0300

    Fix registration validation and streamline photo handling.
    This update improves the sex validation process by supporting additional characters and optimizes photo handling during registration. It also completes the registration wizard by sending a media group message showcasing the user's profile once registration is finished.

--- chathub_bot/bot/scenes/registration.py
+++ chathub_bot/bot/scenes/registration.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from xml.sax import parse
 
@@ -5,8 +6,9 @@ from aiogram import F
 from aiogram.enums import ParseMode
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.scene import on
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.utils.i18n import gettext as _
+from aiogram.utils.media_group import MediaGroupBuilder
 
 from bot import LOGGER
 from bot.scenes.base import BaseSpeedDatingScene
@@ -34,7 +36,6 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
             f'{message.from_user.id}[{step_name}]: {message.text}'
         )
         pg_connector = kwargs['bot'].pg
-        LOGGER.debug(f'Trying to get user {message.from_user.id}...')
         user = await pg_connector.get_user(message.from_user.id)
         if not user:
             # if a user doesn't exist -- starting a registration process
@@ -84,17 +85,23 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
 
         elif step_name == 'sex':
             sex = message.text.upper()
-            if sex not in ['M', 'F']:
+            if sex not in ['M', 'F', 'М', 'Ж']:
                 await message.answer(
                     _('wrong sex'),
                     parse_mode=ParseMode.HTML,
                 )
-            else:
-                await pg_connector.update_user(sex=message.text, user_id=message.from_user.id)
-                await message.answer(
-                    _('invite to enter birthday'),
-                )
-                await state.update_data(step='birthday')
+                return
+            elif sex in ['М', 'Ж']:
+                sex = 'M' if sex == 'М' else 'F'
+            elif sex in ['M', 'F']:
+                # let it be as is
+                pass
+
+            await pg_connector.update_user(sex=sex, user_id=message.from_user.id)
+            await message.answer(
+                _('invite to enter birthday'),
+            )
+            await state.update_data(step='birthday')
 
         elif step_name == 'birthday':
             try:
@@ -120,18 +127,92 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
             )
             await state.update_data(step='photo')
 
-        elif step_name == 'photo':
-            # photo is processed in on_photo method
-            pass
-
     @on.message(F.photo)
-    async def on_photo(self, message: Message, state: FSMContext):
-        ...
+    async def on_photo(self, message: Message, state: FSMContext, **kwargs):
+        data = await state.get_data()
+        step_name = data.get('step', '')
+        if step_name != 'photo':
+            await message.answer(
+                _(f'invite to send {step_name}'),
+                parse_mode=ParseMode.HTML,
+            )
+
+        fm = kwargs['bot'].tfm
+        s3 = kwargs['bot'].s3
+        pg = kwargs['bot'].pg
+
+        photo = message.photo[-1]
+        # Can be useful for monitoring S3 cost
+        # message.photo[-1].file_size
+        photo_info = await message.bot.get_file(photo.file_id)
+        file_extension = photo_info.file_path.split('.')[-1]
+        tmp_file_path = fm.create_temp_file(
+            suffix=file_extension
+        )
+        await message.bot.download(photo, tmp_file_path)
+        s3_path = os.path.join(
+            'user_photos',
+            str(message.from_user.id),
+            f'{int(datetime.now().timestamp())}.{file_extension}'
+        )
+        s3.upload_file(tmp_file_path, s3_path)
+        fm.delete_temp_file(tmp_file_path)
+        await pg.add_image(
+            owner_id=message.from_user.id,  # if user exists verification?
+            s3_bucket=s3.bucket_name,
+            s3_path=s3_path,
+        )
+        await message.answer(
+            _('registration complete'),
+            parse_mode=ParseMode.HTML,
+        )
+        await self.wizard.exit()
 
     @on.message()
     async def on_unknown_message(self, message: Message, state: FSMContext):
         ...
 
     @on.message.exit()
-    async def on_exit(self, message: Message, state: FSMContext):
-        ...
+    async def on_exit(self, message: Message, state: FSMContext, **kwargs):
+        s3 = kwargs['bot'].s3
+        pg = kwargs['bot'].pg
+        fm = kwargs['bot'].tfm
+
+        # get user data to represent
+        user = await pg.get_user(message.from_user.id)
+        if not user:
+            LOGGER.error(
+                f'Registration Scene: cannot find user {message.from_user.id} on exit'
+            )
+            await message.answer(
+                _('internal error'),
+                parse_mode=ParseMode.HTML,
+            )
+        images = await pg.get_images_by_owner(message.from_user.id)
+
+        media_group = MediaGroupBuilder(
+            caption=_('your profile {name} {sex} {age} {city}').format(
+                name=user['name'],
+                sex=_('sex_male') if user['sex'].upper() == 'M' else _('sex_female'),
+                age=datetime.now().year - user['birthday'].year,  # is it correct?
+                city=user['city'],
+            ),
+        )
+
+        # build a message to send
+        tmp_files = []
+        for image in images:
+            tmp_file_path = fm.create_temp_file(
+                suffix=image['s3_path'].split('.')[-1]
+            )
+            tmp_files.append(tmp_file_path)
+            s3.download_file(image['s3_path'], tmp_file_path)
+            media_group.add(type='photo', media=FSInputFile(tmp_file_path))
+        await message.bot.send_media_group(
+            chat_id=message.chat.id,
+            media=media_group.build()
+        )
+
+        # clear temp files
+        for tmp_file in tmp_files:
+            fm.delete_temp_file(tmp_file)
