d597610642fbd449a1571d6badeb64778fc7aac6
Nick Mezhnyakov <mezhng@gmail.com>
2024-08-20 19:09:28 +0300

    Add get_connectors method to BaseSpeedDatingScene
    Enhance scene by providing a static method to retrieve essential connectors from the bot context. This includes AsyncPgConnector, RabbitMQConnector, S3Client, and TempFileManager, facilitating easier access to these resources.

--- chathub_bot/bot/scenes/base.py
+++ chathub_bot/bot/scenes/base.py
@@ -8,11 +8,16 @@ More info:
 https://docs.aiogram.dev/en/dev-3.x/dispatcher/finite_state_machine/scene.html
 """
 import abc
-from typing import Any
+from typing import Any, List
 
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.scene import Scene, on
+from aiogram.fsm.scene import Scene
 from aiogram.types import Message
+
+from bot.tmp_files_manager import TempFileManager
+from chathub_connectors.aws_connectors import S3Client
+from chathub_connectors.postgres_connector import AsyncPgConnector
+from chathub_connectors.rabbitmq_connector import RabbitMQConnector
 
 
 class BaseSpeedDatingScene(Scene, abc.ABC):
@@ -23,3 +28,10 @@ class BaseSpeedDatingScene(Scene, abc.ABC):
     @abc.abstractmethod
     async def on_exit(self, message: Message, state: FSMContext) -> None:
         ...
+
+    @staticmethod
+    def get_connectors(context: dict) -> List[
+        AsyncPgConnector, RabbitMQConnector, S3Client, TempFileManager
+    ]:
+        connectors_list = ['pg', 'rmq', 's3', 'tfm']
+        return [getattr(context['bot'], connector) for connector in connectors_list]
