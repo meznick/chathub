730ae1e6c8d2be23595ab12fc9d462f811d48bcb
Nick Mezhnyakov <mezhng@gmail.com>
2024-08-20 19:09:09 +0300

    Refactor registration scene methods for clarity and modularity
    Refactor existing methods in `registration.py` to improve code organization and readability. Introduced new helper methods `_start_registration`, `_update_user_name`, `_update_user_sex`, `_update_user_birthday`, `_update_user_city`, `_update_user_photo`, `_get_user_profile_data`, and `_send_user_profile` to modularize the different stages of the user registration process. Streamlined the user flow and added detailed docstrings for better maintainability.

--- chathub_bot/bot/scenes/registration.py
+++ chathub_bot/bot/scenes/registration.py
@@ -1,6 +1,5 @@
 import os
 from datetime import datetime
-from xml.sax import parse
 
 from aiogram import F
 from aiogram.enums import ParseMode
@@ -10,9 +9,10 @@ from aiogram.types import Message, FSInputFile
 from aiogram.utils.i18n import gettext as _
 from aiogram.utils.media_group import MediaGroupBuilder
 
-from bot import LOGGER
+from bot import setup_logger
 from bot.scenes.base import BaseSpeedDatingScene
-from chathub_connectors.postgres_connector import AsyncPgConnector
+
+LOGGER = setup_logger(__name__)
 
 
 class RegistrationScene(BaseSpeedDatingScene, state='registration'):
@@ -29,35 +29,33 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
 
     @on.message.enter()
     async def on_enter(self, message: Message, state: FSMContext, **kwargs):
+        """
+        This method is triggered when the user enters the registration scene.
+
+        If the user does not exist, it starts the registration process by calling
+        the `_start_registration` method. If the user already exists, it sends
+        a message to the user indicating that they can edit their profile.
+
+        Note: This method requires a connection to a PostgreSQL database, which
+        is obtained from the `get_connectors` method of a parent class.
+
+        :param message: The message object received from the user.
+        :param state: The FSMContext object for storing and retrieving conversation state.
+        :param kwargs: Additional keyword arguments.
+        """
         data = await state.get_data()
         step_name = data.get('step', '')
         LOGGER.debug(
             f'Registration Scene: '
             f'{message.from_user.id}[{step_name}]: {message.text}'
         )
-        pg_connector = kwargs['bot'].pg
-        user = await pg_connector.get_user(message.from_user.id)
+
+        pg, *_ = self.get_connectors(kwargs)
+
+        user = await pg.get_user(message.from_user.id)
         if not user:
             # if a user doesn't exist -- starting a registration process
-            if step_name == '':
-                await message.answer(
-                    _('welcome message {name}').format(name=message.from_user.full_name),
-                    parse_mode=ParseMode.HTML,
-                )
-                await message.answer(
-                    _('invite to enter name'),
-                    parse_mode=ParseMode.HTML,
-                )
-                await state.update_data(step='name')
-                await pg_connector.add_user(
-                    user_id=message.from_user.id,
-                    username=message.from_user.username
-                )
-            else:
-                LOGGER.warn(
-                    f'User {message.from_user.id} entered '
-                    f'RegistrationScene in state {step_name}'
-                )
+            await self._start_registration(message, pg, state, step_name)
         else:
             # if a user exists - telling him that he can edit his profile
             await message.answer(
@@ -67,80 +65,166 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
 
     @on.message(F.text)
     async def on_message(self, message: Message, state: FSMContext, **kwargs):
+        """
+        This method processes all registration steps except for a photo step.
+
+        :param message: The message object received.
+        :param state: The FSMContext object representing the current state of the conversation.
+        :param kwargs: Additional keyword arguments that may be passed to the method.
+        """
         data = await state.get_data()
         step_name = data.get('step', '')
         LOGGER.debug(
             f'Registration Scene: '
             f'{message.from_user.id}[{step_name}]: {message.text}'
         )
-        pg_connector = kwargs['bot'].pg
 
+        pg, *_ = self.get_connectors(kwargs)
+
+        # processing registration steps
         if step_name == 'name':
-            await pg_connector.update_user(name=message.text, user_id=message.from_user.id)
-            await message.answer(
-                _('invite to enter sex'),
-                parse_mode=ParseMode.HTML,
-            )
-            await state.update_data(step='sex')
+            await self._update_user_name(message, pg, state)
 
         elif step_name == 'sex':
-            sex = message.text.upper()
-            if sex not in ['M', 'F', 'М', 'Ж']:
-                await message.answer(
-                    _('wrong sex'),
-                    parse_mode=ParseMode.HTML,
-                )
-                return
-            elif sex in ['М', 'Ж']:
-                sex = 'M' if sex == 'М' else 'F'
-            elif sex in ['M', 'F']:
-                # let it be as is
-                pass
-
-            await pg_connector.update_user(sex=sex, user_id=message.from_user.id)
-            await message.answer(
-                _('invite to enter birthday'),
-            )
-            await state.update_data(step='birthday')
+            await self._update_user_sex(message, pg, state)
 
         elif step_name == 'birthday':
-            try:
-                input_value = message.text
-                birthday = datetime.strptime(input_value, '%Y-%m-%d').date()
-                await pg_connector.update_user(birthday=birthday, user_id=message.from_user.id)
-                await message.answer(
-                    _('invite to send city'),
-                    parse_mode=ParseMode.HTML,
-                )
-                await state.update_data(step='city')
-            except ValueError:
-                await message.answer(
-                    _('wrong date format'),
-                    parse_mode=ParseMode.HTML,
-                )
+            await self._update_user_birthday(message, pg, state)
 
         elif step_name == 'city':
-            await pg_connector.update_user(city=message.text, user_id=message.from_user.id)
-            await message.answer(
-                _('invite to send photo'),
-                parse_mode=ParseMode.HTML,
-            )
-            await state.update_data(step='photo')
+            await self._update_user_city(message, pg, state)
 
     @on.message(F.photo)
     async def on_photo(self, message: Message, state: FSMContext, **kwargs):
+        """
+        This method processes a photo registration step.
+
+        :param message: The message object received.
+        :param state: The FSMContext object representing the current state of the conversation.
+        :param kwargs: Additional keyword arguments that may be passed to the method.
+        """
         data = await state.get_data()
         step_name = data.get('step', '')
+
+        pg, _, s3, fm = self.get_connectors(kwargs)
+
         if step_name != 'photo':
             await message.answer(
                 _(f'invite to send {step_name}'),
                 parse_mode=ParseMode.HTML,
             )
+        else:
+            await self._update_user_photo(fm, message, pg, s3)
+            await self.wizard.exit()
 
-        fm = kwargs['bot'].tfm
-        s3 = kwargs['bot'].s3
-        pg = kwargs['bot'].pg
+    @on.message()
+    async def on_unknown_message(self, message: Message, state: FSMContext):
+        """
+        Handles the event when an unknown message is received while registering user.
 
+        :param message: The unknown message received.
+        :param state: The current state of the finite state machine.
+        """
+        ...
+
+    @on.message.exit()
+    async def on_exit(self, message: Message, state: FSMContext, **kwargs):
+        """
+        This method is triggered when the user exists the registration scene.
+        On exiting (successful registration) user's profile is sent.
+
+        :param message: The message object related to the event.
+        :param state: The FSMContext object representing the state machine context.
+        :param kwargs: Additional keyword arguments.
+        :return: None
+
+        """
+        pg, _, s3, fm = self.get_connectors(kwargs)
+
+        user, images = await self._get_user_profile_data(pg, message)
+
+        await self._send_user_profile(fm, images, message, s3, user)
+
+    @staticmethod
+    async def _start_registration(message, pg, state, step_name):
+        if step_name == '':
+            await message.answer(
+                _('welcome message {name}').format(name=message.from_user.full_name),
+                parse_mode=ParseMode.HTML,
+            )
+            await message.answer(
+                _('invite to enter name'),
+                parse_mode=ParseMode.HTML,
+            )
+            await state.update_data(step='name')
+            await pg.add_user(
+                user_id=message.from_user.id,
+                username=message.from_user.username
+            )
+        else:
+            await message.answer(
+                _(f'invite to send {step_name}'),
+                parse_mode=ParseMode.HTML,
+            )
+
+    @staticmethod
+    async def _update_user_name(message, pg, state):
+        await pg.update_user(name=message.text, user_id=message.from_user.id)
+        await message.answer(
+            _('invite to enter sex'),
+            parse_mode=ParseMode.HTML,
+        )
+        await state.update_data(step='sex')
+
+    @staticmethod
+    async def _update_user_sex(message, pg, state):
+        sex = message.text.upper()
+        if sex not in ['M', 'F', 'М', 'Ж']:
+            await message.answer(
+                _('wrong sex'),
+                parse_mode=ParseMode.HTML,
+            )
+            return
+        elif sex in ['М', 'Ж']:
+            sex = 'M' if sex == 'М' else 'F'
+        elif sex in ['M', 'F']:
+            # let it be as is
+            pass
+
+        await pg.update_user(sex=sex, user_id=message.from_user.id)
+        await message.answer(
+            _('invite to enter birthday'),
+        )
+        await state.update_data(step='birthday')
+
+    @staticmethod
+    async def _update_user_city(message, pg, state):
+        await pg.update_user(city=message.text, user_id=message.from_user.id)
+        await message.answer(
+            _('invite to send photo'),
+            parse_mode=ParseMode.HTML,
+        )
+        await state.update_data(step='photo')
+
+    @staticmethod
+    async def _update_user_birthday(message, pg, state):
+        try:
+            input_value = message.text
+            birthday = datetime.strptime(input_value, '%Y-%m-%d').date()
+            await pg.update_user(birthday=birthday, user_id=message.from_user.id)
+            await message.answer(
+                _('invite to send city'),
+                parse_mode=ParseMode.HTML,
+            )
+            await state.update_data(step='city')
+        except ValueError:
+            await message.answer(
+                _('wrong date format'),
+                parse_mode=ParseMode.HTML,
+            )
+
+    @staticmethod
+    async def _update_user_photo(fm, message, pg, s3):
         photo = message.photo[-1]
         # Can be useful for monitoring S3 cost
         # message.photo[-1].file_size
@@ -166,18 +250,9 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
             _('registration complete'),
             parse_mode=ParseMode.HTML,
         )
-        await self.wizard.exit()
 
-    @on.message()
-    async def on_unknown_message(self, message: Message, state: FSMContext):
-        ...
-
-    @on.message.exit()
-    async def on_exit(self, message: Message, state: FSMContext, **kwargs):
-        s3 = kwargs['bot'].s3
-        pg = kwargs['bot'].pg
-        fm = kwargs['bot'].tfm
-
+    @staticmethod
+    async def _get_user_profile_data(pg, message):
         # get user data to represent
         user = await pg.get_user(message.from_user.id)
         if not user:
@@ -188,8 +263,13 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
                 _('internal error'),
                 parse_mode=ParseMode.HTML,
             )
+            return
         images = await pg.get_images_by_owner(message.from_user.id)
 
+        return user, images
+
+    @staticmethod
+    async def _send_user_profile(fm, images, message, s3, user):
         media_group = MediaGroupBuilder(
             caption=_('your profile {name} {sex} {age} {city}').format(
                 name=user['name'],
@@ -199,7 +279,6 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
             ),
         )
 
-        # build a message to send
         tmp_files = []
         for image in images:
             tmp_file_path = fm.create_temp_file(
@@ -212,7 +291,6 @@ class RegistrationScene(BaseSpeedDatingScene, state='registration'):
             chat_id=message.chat.id,
             media=media_group.build()
         )
-
         # clear temp files
         for tmp_file in tmp_files:
             fm.delete_temp_file(tmp_file)
