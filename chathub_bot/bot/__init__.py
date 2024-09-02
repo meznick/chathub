34b40693c903b976d82475cbdd8bb005a1a3068a
Nick Mezhnyakov <mezhng@gmail.com>
2024-09-02 19:21:36 +0300

    Update default environment variables for broker and DB credentials
    Changed default values for RabbitMQ and PostgreSQL credentials from placeholders to empty strings. This ensures sensitive information is not inadvertently left in the codebase.

--- chathub_bot/bot/__init__.py
+++ chathub_bot/bot/__init__.py
@@ -25,20 +25,20 @@ TG_TOKEN = os.getenv('TG_BOT_TOKEN', '')
 MESSAGE_BROKER_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
 MESSAGE_BROKER_PORT = int(os.getenv('RABBITMQ_PORT', '5672'))
 MESSAGE_BROKER_VIRTUAL_HOST = os.getenv('RABBITMQ_VIRTUAL_HOST', '/')
-MESSAGE_BROKER_EXCHANGE = os.getenv('TG_BOT_RABBITMQ_EXCHANGE', 'default_exchange')
-MESSAGE_BROKER_QUEUE = os.getenv('TG_BOT_RABBITMQ_QUEUE', 'default_queue')
+MESSAGE_BROKER_EXCHANGE = os.getenv('TG_BOT_RABBITMQ_EXCHANGE', '')
+MESSAGE_BROKER_QUEUE = os.getenv('TG_BOT_RABBITMQ_QUEUE', '')
 MESSAGE_BROKER_ROUTING_KEY = os.getenv(
     'TG_BOT_RABBITMQ_ROUTING_KEY',
-    'default_routing_key'
+    ''
 )
-MESSAGE_BROKER_USERNAME = os.getenv('RABBITMQ_USERNAME', 'guest')
-MESSAGE_BROKER_PASSWORD = os.getenv('RABBITMQ_PASSWORD', 'guest')
+MESSAGE_BROKER_USERNAME = os.getenv('TG_BOT_RABBITMQ_USERNAME', '')
+MESSAGE_BROKER_PASSWORD = os.getenv('TG_BOT_RABBITMQ_PASSWORD', '')
 # Read Postgres credentials from environment
 POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
 POSTGRES_PORT = int(os.getenv('POSTGRES_PORT', '5432'))
-POSTGRES_DB = os.getenv('POSTGRES_DB', 'chathub')
-POSTGRES_USER = os.getenv('POSTGRES_USER', 'guest')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'guest')
+POSTGRES_DB = os.getenv('POSTGRES_DB', '')
+POSTGRES_USER = os.getenv('POSTGRES_USER', '')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', '')
 # Read AWS credentials from environment variables
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
