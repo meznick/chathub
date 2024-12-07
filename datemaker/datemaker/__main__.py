9ecb7a19cdb14f6c3decc823d0138775f875fccb
Nick Mezhnyakov <mezhng@gmail.com>
2024-12-07 11:53:32 +0300

    Add Google Meet credentials to DateMakerService instantiation
    This commit includes MEET_CREDS_FILE and MEET_TOKEN_FILE parameters in the DateMakerService object within the __main__.py file. These changes ensure that the service can properly authenticate and interact with Google Meet, enhancing its functionality.

--- datemaker/datemaker/__main__.py
+++ datemaker/datemaker/__main__.py
@@ -2,13 +2,23 @@
 main file for calling module
 """
 import argparse
-import asyncio
 
 from datemaker import (
-    MESSAGE_BROKER_HOST, MESSAGE_BROKER_PORT,
-    MESSAGE_BROKER_VIRTUAL_HOST, MESSAGE_BROKER_EXCHANGE, MESSAGE_BROKER_QUEUE,
-    MESSAGE_BROKER_ROUTING_KEY, MESSAGE_BROKER_USERNAME, MESSAGE_BROKER_PASSWORD,
-    POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD
+    MESSAGE_BROKER_HOST,
+    MESSAGE_BROKER_PORT,
+    MESSAGE_BROKER_VIRTUAL_HOST,
+    MESSAGE_BROKER_EXCHANGE,
+    MESSAGE_BROKER_QUEUE,
+    MESSAGE_BROKER_ROUTING_KEY,
+    MESSAGE_BROKER_USERNAME,
+    MESSAGE_BROKER_PASSWORD,
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+    POSTGRES_DB,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    MEET_TOKEN_FILE,
+    MEET_CREDS_FILE
 )
 from .service import DateMakerService
 
@@ -23,7 +33,8 @@ if __name__ == "__main__":
 
     service = DateMakerService(
         # all parameters from GoogleMeetApiController
-
+        meet_creds_file=MEET_CREDS_FILE,
+        meet_token_file=MEET_TOKEN_FILE,
         # all parameters from RabbitMQConnector (0.0.3)
         message_broker_host=MESSAGE_BROKER_HOST,
         message_broker_port=MESSAGE_BROKER_PORT,
