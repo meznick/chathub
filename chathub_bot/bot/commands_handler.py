3f1d78a78571039e0337f244048aadf29b370884
Nick Mezhnyakov <mezhng@gmail.com>
2024-12-08 14:53:28 +0300

    Add new bot commands for sending messages and updates.
    This commit introduces several new bot commands, including sending meeting invitations and ready-for-event requests. The changes involve handling new command messages, importing json for message processing, and updating the parse mode to MARKDOWN_V2 for better text formatting.

--- chathub_bot/bot/commands_handler.py
+++ chathub_bot/bot/commands_handler.py
@@ -1,3 +1,5 @@
+import json
+
 from aiogram.enums import ParseMode
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
@@ -15,8 +17,24 @@ class BotCommandsHandlerMixin:
             await self.request_event_registration_confirmation(headers, user_id)
         elif BotCommands.SEND_RULES.value in message:
             await self.send_pre_event_rules(user_id)
+        elif BotCommands.INVITE_TO_MEETING.value in message:
+            await self.send_meeting_invitation(message, headers)
+        elif BotCommands.SEND_PARTNER_PROFILE.value in message:
+            ...
+        elif BotCommands.SEND_PARTNER_RATING_REQUEST.value in message:
+            ...
+        elif BotCommands.SEND_PARTNER_PROFILE_VERIFICATION_REQUEST.value in message:
+            ...
+        elif BotCommands.SEND_FINAL_DATING_MESSAGE.value in message:
+            ...
+        elif BotCommands.SEND_MATCH_MESSAGE.value in message:
+            ...
+        elif BotCommands.SEND_READY_FOR_EVENT_REQUEST.value in message:
+            await self.send_ready_for_event_request(message, headers)
+        elif BotCommands.SEND_BREAK_MESSAGE.value in message:
+            ...
         else:
-            raise UnknownBotCommandError(f'Cannot process command, message: {message[:30]}')
+            raise UnknownBotCommandError(f'Cannot process command, message: {message[:100]}')
         return True
 
     async def request_event_registration_confirmation(self, headers: dict, user_id: int):
@@ -51,10 +69,41 @@ class BotCommandsHandlerMixin:
         await self.send_message(
             chat_id=user_id,
             text=_('event will start soon, heres rule remainder'),
-            parse_mode=ParseMode.HTML,
+            parse_mode=ParseMode.MARKDOWN_V2,
         )
         await self.send_message(
             chat_id=user_id,
             text=_('dating rules'),
-            parse_mode=ParseMode.HTML,
+            parse_mode=ParseMode.MARKDOWN_V2,
+        )
+
+    async def send_meeting_invitation(self, message: str, headers: dict):
+        _ = self.i18n.gettext
+
+        data = json.loads(message)[BotCommands.INVITE_TO_MEETING.value]
+
+        await self.send_message(
+            chat_id=headers.get('user_id'),
+            text=_('meeting invitation {url}'.format(url=data.get('url'))),
+            parse_mode=ParseMode.MARKDOWN_V2,
+        )
+
+    async def send_ready_for_event_request(self, message: str, headers: dict):
+        _ = self.i18n.gettext
+
+        builder = InlineKeyboardBuilder()
+        builder.button(
+            text=_('i am ready button'),
+            callback_data=DatingEventCallbackData(
+                action=DatingEventActions.READY.value,
+                event_id=headers.get('event_id'),
+                user_id=headers.get('user_id'),
+                confirmed=False,
+            ))
+
+        await self.send_message(
+            chat_id=headers.get('user_id'),
+            text=_('are you ready to start event?'),
+            parse_mode=ParseMode.MARKDOWN_V2,
+            reply_markup=builder.as_markup(),
         )
