d0ba2c5ffc2db9feaf0e3a36662ce0dab4c1f129
Nick Mezhnyakov <mezhng@gmail.com>
2024-12-18 21:24:18 +0300

    Some updates on translations.

--- chathub_bot/bot/utils.py
+++ chathub_bot/bot/utils.py
@@ -2,5 +2,5 @@ import re
 
 
 def escape_markdown_v2(text: str) -> str:
-    reserved_characters = r'[_\*\[\]\(\)~`>#+\-=|{}.!]'
+    reserved_characters = r'[_\[\]\(\)~`>#+\-=|{}.!]'
     return re.sub(reserved_characters, r'\\\g<0>', text)
