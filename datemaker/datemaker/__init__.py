b0e878c04619a052af080185786fc6ba2ff25677
Nick Mezhnyakov <mezhng@gmail.com>
2025-01-23 11:09:21 +0300

    Add RABBITMQ_EXCHANGE constant to configuration
    Introduce `RABBITMQ_EXCHANGE` with a default value 'chathub_direct_main' for configuration settings. This prepares the system for RabbitMQ exchange integration.

--- datemaker/datemaker/__init__.py
+++ datemaker/datemaker/__init__.py
@@ -91,3 +91,4 @@ POSTGRES_USER = os.getenv('POSTGRES_USER', '')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', '')
 
 TG_BOT_ROUTING_KEY = 'tg_bot_dev' if DEBUG.lower() == 'true' else 'tg_bot_prod'
+RABBITMQ_EXCHANGE = 'chathub_direct_main'
